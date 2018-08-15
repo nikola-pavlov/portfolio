c1bf1c46fe27a577da6ac2b544afa9143dfa8d72
Nikola <nikola8989@hotmail.com>
2018-08-16 01:13:20 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require ("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+// app.listen(process.env.PORT, process.env.IP);
+
+
+app.listen(3000, "127.0.0.1", function(){
+	console.log("Server started!");
+});
