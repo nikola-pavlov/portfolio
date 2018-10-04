02f0e550f98c8021289e3acb9ee4fe57e6e6df3f
Nikola <nikola8989@hotmail.com>
2018-10-04 20:28:04 +0200

    Reveal elements on scroll

--- public/assets/scripts/App.js
+++ public/assets/scripts/App.js
@@ -1,3 +1,9 @@
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".service-item"), "85%");
+new RevealOnScroll($(".mobile-ready-item"), "75%");
+new RevealOnScroll($(".technologies__content"), "95%");
+new RevealOnScroll($(".recent-work"), "60%");
