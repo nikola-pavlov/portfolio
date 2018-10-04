811059f408e1fc417b857ad95c1fdc9f7e02a17b
Nikola <nikola8989@hotmail.com>
2018-10-05 00:58:00 +0200

    build sticky header

--- public/assets/scripts/App.js
+++ public/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
+import StickyHeader from "./modules/StickyHeader";
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".service-item"), "85%");
 new RevealOnScroll($(".mobile-ready-item"), "75%");
 new RevealOnScroll($(".technologies__content"), "95%");
 new RevealOnScroll($(".recent-work"), "60%");
+var stickyHeader = new StickyHeader();
