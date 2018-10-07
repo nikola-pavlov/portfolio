e14696b3f434bd1b87261159e9e79926d22f04c2
Nikola <nikola8989@hotmail.com>
2018-10-08 01:28:03 +0200

    complete lazy loading of images

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: {
-    app: './public/assets/scripts/App.js'
+    app: "./public/assets/scripts/App.js",
+    vendor: "./public/assets/scripts/Vendor.js"
   },
   output: {
-    filename: 'App.js',
-    path: path.resolve(__dirname, './public/temp/scripts')
+    filename: "[name].js",
+    path: "./public/temp/scripts"
+    // path: path.resolve(__dirname, "./public/temp/scripts")
   }
 };
